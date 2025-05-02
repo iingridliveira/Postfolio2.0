b5b7c727e46aeaae404ab1954f2b4e0a4a49443d
Ingridoliveira <oligrid20@gmail.com>
2025-05-02 19:09:07 -0300

    fix: terminei a nav bar

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/Logo2.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -10,20 +10,31 @@ export const Footer = () => {
     <footer className="footer py-5">
       <Container>
         <Row className="align-items-center">
-  
           <Col size={12} sm={6}>
             <img src={logo} alt="Logotipo do rodapé" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Ícone de rodapé 1" /></a>
-              <a href="#"><img src={navIcon2} alt="Ícone de rodapé 2" /></a>
-              <a href="#"><img src={navIcon3} alt="Ícone de rodapé 3" /></a>
+              <a href="https://www.linkedin.com/in/ingrid-oliveiradev/" target="_blank">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a
+                href="https://www.facebook.com/share/1AQLigZUaK"
+                target="_blank"
+              >
+                <img src={navIcon2} alt="" />
+              </a>
+              <a
+                href="https://www.instagram.com/ingridoliveira_sn"
+                target="_blank"
+              >
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
+  );
 }
--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/Logo3.png';
+import logo from '../assets/img/Logo2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -37,30 +37,74 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo}  style={{ width:"1000px;"}} alt="Logotipo principal" />
+            <img
+              src={logo}
+              style={{ width: "180px", height: "auto" }}
+              alt="Logotipo principal"
+            />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Início</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projetos</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Início
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Habilidades
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projetos
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/ingrid-oliveiradev/" target="__blank">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a
+                  href="https://www.facebook.com/share/1AQLigZUaK"
+                  target="__blank"
+                >
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a
+                  href="https://www.instagram.com/ingridoliveira_sn"
+                  target="__blank"
+                >
+                  <img src={navIcon3} alt="" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Vamos Conectar</span></button>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Vamos nos Conectar</span>
+                </button>
               </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
+  );
 }
